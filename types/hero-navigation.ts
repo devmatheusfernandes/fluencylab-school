ceb7b3b396602ee00a2fa52d2de97b78f866c67e
devmatheusfernandes <dev.matheusfernandes@gmail.com>
2025-10-26 00:37:04 -0300

    landing page initial state/idea/

---
+++ types/hero-navigation.ts
@@ -0,0 +1,8 @@
+export const navigation = [
+  { name: "Sobre Nós", href: "#aboutus" },
+  { name: "Nosso Time", href: "#ourteam" },
+  { name: "Perguntas Frequentes", href: "#faq" },
+  { name: "Games", href: "/games" },
+  { name: "Nivelamento", href: "/u/placement" },
+  { name: "Libras", href: "/libras" },
+];
