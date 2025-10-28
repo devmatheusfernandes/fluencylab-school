84b3987a7d58f18f7894f67554565567c96466ea
devmatheusfernandes <dev.matheusfernandes@gmail.com>
2025-10-28 12:04:20 -0300

    i18n fixed and instructions on how to use created

--- proxy.ts
+++ proxy.ts
@@ -3,8 +3,9 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
   locales: ["en", "pt"],
   defaultLocale: "pt",
+  localePrefix: "always",
 });
 
 export const config = {
-  matcher: ["/((?!api|_next|.*\\..*).*)"],
+  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
 };
