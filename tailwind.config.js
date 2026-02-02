7a070d99b85d2cc3a01004f24e7dae9aecec817d
devmatheusfernandes <dev.matheusfernandes@gmail.com>
2026-02-02 12:35:07 -0300

    PWA improvements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export const content = [
-  './pages/**/*.{js,ts,jsx,tsx,mdx}',
-  './components/**/*.{js,ts,jsx,tsx,mdx}',
-  './app/**/*.{js,ts,jsx,tsx,mdx}',
+  "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./app/**/*.{js,ts,jsx,tsx,mdx}",
 ];
-export const darkMode = 'class';
+export const darkMode = "class";
 export const theme = {
   extend: {
     animation: {
       shimmer: "var(--animate-shimmer)",
+    },
+    screens: {
+      standalone: { raw: "(display-mode: standalone)" },
     },
   },
 };
